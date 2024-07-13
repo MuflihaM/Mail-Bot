10116283058c94926a06ee29b4b5ac55a5a136df
MuflihaM <86520863+MuflihaM@users.noreply.github.com>
2024-07-13 19:37:00 +0530

    Add files via upload

---
+++ Scenario 2.py
@@ -0,0 +1,10 @@
+import smtplib 
+main = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+send = 'elayarajameenaloshini002@gmail.com'
+password = 'rxjl uxdw qyjm tvow'
+rec = 'sit20ad009@sairamtap.edu.in'
+Msg = 'Testing mail'
+main.login(send, password)
+main.sendmail(send, rec, Msg)
+print('Mail sent')
+main.quit()
